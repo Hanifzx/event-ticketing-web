fd99a4732a6e357a7e916c11f29aef78325640c7
Hanifzx <muh.hanief.1974@gmail.com>
2025-11-15 16:10:52 +0800

    feat: add routes and controller for custom organizer registration

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\OrganizerRegistrationController;
 
 Route::view('/', 'welcome');
+
+// Menampilkan halaman form registrasi organizer
+Route::get('/register-organizer', [OrganizerRegistrationController::class, 'create'])
+        ->middleware('guest')
+        ->name('organizer.register');
+
+// Proses registrasi organizer
+Route::post('/register-organizer', [OrganizerRegistrationController::class, 'store'])
+        ->middleware('guest');
 
 Route::get('/dashboard', [HomeController::class, 'index'])
     ->middleware(['auth', 'verified'])
